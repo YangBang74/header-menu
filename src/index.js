b089369b5fc8cf136adb3c9d9d72cfa27618e709
Yan <crowbeat.47@gmail.com>
2025-07-04 14:37:43 +0500

    feat: enhance hero section with layout, text content, and background image

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './assets/styles/main.scss'
+
+// import heroImage from './assets/img/gamepad.png'
+
+// const imgContainer = document.querySelector('.hero__body-img')
+// const img = document.createElement('img')
+// img.src = heroImage
+// img.alt = 'Gamepad'
+// imgContainer.appendChild(img)
