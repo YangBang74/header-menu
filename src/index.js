b54cf7b4f6a710a387d78b5bfef5a7cca42c66fa
Yan <crowbeat.47@gmail.com>
2025-07-04 15:24:46 +0500

    feat: remove commented-out code for image import and handling in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
 import './assets/styles/main.scss'
-
-// import heroImage from './assets/img/gamepad.png'
-
-// const imgContainer = document.querySelector('.hero__body-img')
-// const img = document.createElement('img')
-// img.src = heroImage
-// img.alt = 'Gamepad'
-// imgContainer.appendChild(img)
